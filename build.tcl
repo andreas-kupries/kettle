3dc9521b71984ebf45386e3a692590d18cf4d520
Andreas Kupries <akupries@shaw.ca>
2012-09-23 07:38:00 +0000

    Special code paths for self-installation of kettle

--- build.tcl
+++ build.tcl
@@ -1,3 +1,6 @@
 #!kettle -f
-kettle tclapp kettle
+# Special code: Direct load of the relevant packages.
+# For ourselves we can't assume that we are installed, and even if so,
+# it may an older, unusable version.
+source [kettle path tclapp.tcl] ; kettle tclapp kettle
 #kettle tcl
