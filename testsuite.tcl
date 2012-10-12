7e7214df14e765e230c22e16800425cd370d9efa
Andreas Kupries <akupries@shaw.ca>
2012-10-12 20:00:08 +0000

    Refactored the test suite handling into helper commands for main execution, and handling of lines, for readability. Added recursive invokation of self for installation (debug prefered, regular as fallback) of the package under test in a local place.

--- testsuite.tcl
+++ testsuite.tcl
@@ -25,106 +25,145 @@ proc ::kettle::testsuite {{testsrcdir tests}} {
 	Run the testsuite
     } {testsrcdir testsuite} {
 
-	# Test dependencies - building them is not covered here!
+	# Note: We build and install the package under test (and its
+	# dependencies (TODO)) into a local directory (in the current
+	# working directory). We try to install a debug variant first,
+	# and if that fails a regular one. This is important for
 
-	# We are running each test file in a separate sub process, to
-	# catch crashes, etc. ... We assume that the test file is self
-	# contained in terms of loading all its dependencies, like
-	# tcltest itself, utility commands it may need, etc. This
-	# assumption allows us to run it directly, using our own
-	# tcl executable as interpreter.
-
-	# options for tcltest. (l => line information for failed tests).
-
-	# Note: See tcllib's sak.tcl for a more mature and featureful
-	# system of running a testsuite and postprocessing results.
-
-	package require struct::matrix
-
-	struct::matrix M
-	M add columns 5
-	M add row {File Total Passed Skipped Failed}
-
-	set ctotal   0
-	set cpassed  0
-	set cskipped 0
-	set cfailed  0
-
-	path in $testsrcdir {
-	    foreach test $testsuite {
-		io note { io puts ${test}... }
-
-		path pipe line {
-
-		    # This first draft uses the simple output
-		    # processing logic I put into older build.tcl
-		    # scripts (pre-kettle). This should be replaced
-		    # with the sak.tcl testsuite support found in
-		    # tcllib/tklib.
-
-		    # TODO ## test --log option ?
-		    #io puts $log $line ; # Full log.
-
-		    if {[string match "++++*"      $line] ||
-			[string match "----*start" $line]} {
-			# Flash report of activity...
-			io interm {
-			    io puts -nonewline "\r$line                                  "
-			    flush stdout
-			}
-			io ingui {
-			    io puts $line
-			}
-			continue
-		    }
-
-		    # Failed tests are reported immediately, in full.
-		    if {[string match {*error: test failed*} $line]} {
-
-			# shorten the shown path for the test file.
-			set r [lassign [split $line :] path]
-			set line [join [linsert $r 0 [file tail $path]] :]
-			set line [string map {{error: test } {}} $line]
-
-			io err {
-			    io interm {
-				io puts \r$line\t\t
-				flush stdout
-			    }
-			    io ingui {
-				io puts $line
-			    }
-			}
-			status fail
-			continue
-		    }
-
-		    # Collect the statistics (per .test file).
-		    if {![string match *Total* $line]} continue
-
-		    lassign $line file _ total _ passed _ skipped _ failed
-		    if {$failed}  { set failed  " $failed"  } ; # indent, stand out.
-		    if {$skipped} { set skipped " $skipped" } ; # indent, stand out.
-		    M add row [list $file $total $passed $skipped $failed]
-
-		    incr ctotal   $total
-		    incr cpassed  $passed
-		    incr cskipped $skipped
-		    incr cfailed  $failed
-
-		} [info nameofexecutable] $test -verbose bpstenl
+	set tmp .kettle_test_install ;#[path tmpfile .kettle_test_install_]
+	try {
+	    if {![invoke self debug   --prefix $tmp] &&
+		![invoke self install --prefix $tmp]
+	    } {
+		status fail "Unable to generate local test installation"
 	    }
+
+	    # NOTE/TODO If we use a dynamically named test directory
+	    # we will have to inject this information into the test
+	    # suite, somehow. Otherwise we can hardwire the directory
+	    # into the test support code, or document it that way.
+
+	    TestRun $testsrcdir $testsuite
+	} finally {
+	    file delete -force $tmp
 	}
-
-	M add row {File Total Passed Skipped Failed}
-	M add row [list {} $ctotal $cpassed $cskipped $cfailed]
-
-	io puts "\n"
-	io puts [M format 2string]
-	io puts ""
-
     } $root $testsuite
 
+    return
+}
+
+# # ## ### ##### ######## ############# #####################
+## Support code for the recipe.
+
+proc ::kettle::TestRun {srcdir testfiles} {
+
+    # We are running each test file in a separate sub process, to
+    # catch crashes, etc. ... We assume that the test file is self
+    # contained in terms of loading all its dependencies, like
+    # tcltest itself, utility commands it may need, etc. This
+    # assumption allows us to run it directly, using our own
+    # tcl executable as interpreter.
+
+    # options for tcltest. (l => line information for failed tests).
+
+    # Note: See tcllib's sak.tcl for a more mature and featureful
+    # system of running a testsuite and postprocessing results.
+
+    package require struct::matrix
+
+    struct::matrix M
+    M add columns 5
+    M add row {File Total Passed Skipped Failed}
+
+    set ctotal   0  ; # These counteres are all updated in
+    set cpassed  0  ; # TestProcessLine.
+    set cskipped 0  ; #
+    set cfailed  0  ; #
+    set status   ok ; # May change to 'fail' in TestProcessLine.
+
+    path in $srcdir {
+	foreach test $testfiles {
+	    io note { io puts ${test}... }
+
+	    path pipe line {
+		TestProcessLine $line
+	    } [info nameofexecutable] $test -verbose bpstenl
+	}
+    }
+
+    M add row {File Total Passed Skipped Failed}
+    M add row [list {} $ctotal $cpassed $cskipped $cfailed]
+
+    io puts "\n"
+    io puts [M format 2string]
+    io puts ""
+
+    # Report ok/fail
+    status $status
+    return
+}
+
+proc ::kettle::TestProcessLine {line} {
+    # Counters and other state in the calling environment.
+    upvar 1 ctotal ctotal cpassed cpassed \
+	cskipped cskipped cfailed cfailed \
+	status status
+
+    # This first draft uses the simple output
+    # processing logic I put into older build.tcl
+    # scripts (pre-kettle). This should be replaced
+    # with the sak.tcl testsuite support found in
+    # tcllib/tklib.
+
+    # TODO ## test --log option ?
+    #io puts $log $line ; # Full log.
+
+    if {[string match "++++*"      $line] ||
+	[string match "----*start" $line]} {
+	# Flash report of activity...
+	io interm {
+	    io puts -nonewline "\r$line                                  "
+	    flush stdout
+	}
+	io ingui {
+	    io puts $line
+	}
+	return
+    }
+
+    # Failed tests are reported immediately, in full.
+    if {[string match {*error: test failed*} $line]} {
+
+	# shorten the shown path for the test file.
+	set r [lassign [split $line :] path]
+	set line [join [linsert $r 0 [file tail $path]] :]
+	set line [string map {{error: test } {}} $line]
+
+	io err {
+	    io interm {
+		io puts \r$line\t\t
+		flush stdout
+	    }
+	    io ingui {
+		io puts $line
+	    }
+	}
+	set status fail
+	return
+    }
+
+    # Collect the statistics (per .test file).
+    if {![string match *Total* $line]} return
+
+    lassign $line file _ total _ passed _ skipped _ failed
+    if {$failed}  { set failed  " $failed"  } ; # indent, stand out.
+    if {$skipped} { set skipped " $skipped" } ; # indent, stand out.
+    M add row [list $file $total $passed $skipped $failed]
+
+    incr ctotal   $total
+    incr cpassed  $passed
+    incr cskipped $skipped
+    incr cfailed  $failed
     return
 }
 
