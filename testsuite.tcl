41d2ba3c4c4a59722ac0b84e82d3851ffcd9d0ba
Andreas Kupries <akupries@shaw.ca>
2012-10-18 00:55:52 +0000

    Bulk copy of the tcllib/sak/test code, plus partial integration work.

--- testsuite.tcl
+++ testsuite.tcl
@@ -103,6 +103,7 @@ proc ::kettle::Test::Run {srcdir testfiles localprefix} {
     # system of running a testsuite and postprocessing results.
 
     LogBegin
+    LogE ============================================================
 
     package require struct::matrix
 
@@ -110,7 +111,7 @@ proc ::kettle::Test::Run {srcdir testfiles localprefix} {
     M add columns 5
     M add row {File Total Passed Skipped Failed}
 
-    set ctotal   0  ; # These counteres are all updated in
+    set ctotal   0  ; # These counters are all updated in
     set cpassed  0  ; # ProcessLine.
     set cskipped 0  ; #
     set cfailed  0  ; #
@@ -120,6 +121,7 @@ proc ::kettle::Test::Run {srcdir testfiles localprefix} {
 
     path in $srcdir {
 	foreach test $testfiles {
+	    # change next to log/log
 	    io note { io puts ${test}... }
 
 	    path pipe line {
@@ -133,6 +135,9 @@ proc ::kettle::Test::Run {srcdir testfiles localprefix} {
 	}
     }
 
+
+
+
     M add row {File Total Passed Skipped Failed}
     M add row [list {} $ctotal $cpassed $cskipped $cfailed]
 
@@ -141,6 +146,48 @@ proc ::kettle::Test::Run {srcdir testfiles localprefix} {
     io puts ""
 
     LogDone
+
+    if {0} {
+
+
+    puts $logext "Passed  [format %6d $pass] of [format %6d $total]"
+    puts $logext "Skipped [format %6d $skip] of [format %6d $total]"
+
+    if {$fail} {
+	puts $logext "Failed  [red][format %6d $fail][rst] of [format %6d $total]"
+    } else {
+	puts $logext "Failed  [format %6d $fail] of [format %6d $total]"
+    }
+    if {$err} {
+	puts $logext "#Errors [mag][format %6d $err][rst]"
+    } else {
+	puts $logext "#Errors [format %6d $err]"
+    }
+
+    if {$alog} {
+	variable xtimes
+	array set times $xtimes
+
+	struct::matrix M
+	M add columns 6
+	foreach k [lsort -dict [array names times]] {
+	    #foreach {shell module testfile} $k break
+	    foreach {testnum delta score} $times($k) break
+	    M add row [linsert $k end $testnum $delta $score]
+	}
+	M sort rows -decreasing 5
+
+	M insert row 0 {Shell Module Testsuite Tests Seconds uSec/Test}
+	M insert row 1 {===== ====== ========= ===== ======= =========}
+	M add    row   {===== ====== ========= ===== ======= =========}
+
+	puts $logsum \nTimings...
+	puts $logsum [M format 2string]
+    }
+
+
+    }
+
     # Report ok/fail
     status $status
     return
@@ -152,6 +199,59 @@ proc ::kettle::Test::ProcessLine {line} {
 	cskipped cskipped cfailed cfailed \
 	status status
 
+    set line [string trimright $line]
+
+    LogE $line
+
+    set rline $line
+    set line [string trim $line]
+    if {[string equal $line ""]} return
+
+    # Recognize various parts written by the sub-shell and act on
+    # them. If a line is recognized and acted upon the remaining
+    # matchers are _not_ executed.
+
+    Host;Platform;Cwd;Shell;Tcl
+    #Start
+    #End
+    #StartFile
+    #EndFile
+    #Testsuite
+    #NoTestsuite
+    #Support;Testing;Other
+    #Summary
+    #CaptureFailureSync            ; # xcollect 1 => 2
+    #CaptureFailureCollectBody     ; # xcollect 2 => 3 => 5
+    #CaptureFailureCollectActual   ; # xcollect 3 => 4
+    #CaptureFailureCollectExpected ; # xcollect 4 => 0
+    #CaptureFailureCollectError    ; # xcollect 5 => 0
+    #CaptureStackStart
+    #CaptureStack
+
+    #TestStart
+    #TestSkipped
+    #TestPassed
+    #TestFailed                    ; # xcollect => 1
+
+    #SetupError
+    #Aborted
+    #AbortCause
+
+    Match||Skip||Sourced
+    # Unknown lines are printed
+    #if {!$araw} {puts !$line}
+
+
+
+
+
+
+
+
+
+
+
+return
     # This first draft uses the simple output
     # processing logic I put into older build.tcl
     # scripts (pre-kettle). This should be replaced
@@ -212,6 +312,12 @@ proc ::kettle::Test::ProcessLine {line} {
 
 # # ## ### ##### ######## ############# #####################
 
+proc ::kettle::Test::LogE {text} {
+    if {[option get --log-mode] eq "compact"} return
+    Log log $text
+    return
+}
+
 proc ::kettle::Test::Log {name text} {
     variable stream
     {*}[dict get $stream $name] $text
@@ -225,7 +331,7 @@ proc ::kettle::Test::LogBegin {} {
     if {[option get --log-mode] eq "files"} {
 	set stem [option get --log]
 	foreach name $streams {
-	    dict set stream $name [list ::puts [open $stem.$name] w]
+	    dict set stream $name [list ::puts [open $stem.$name w]]
 	}
     } else {
 	dict set stream log [list ::puts stdout]
@@ -249,6 +355,555 @@ proc ::kettle::Test::LogDone {} {
 
 # Writer for disabled log streams.
 proc ::kettle::Test::LogNull {args} {}
+
+# # ## ### ##### ######## ############# #####################
+
+proc ::kettle::Test::Host {} {
+    upvar 1 line line
+    if {![regexp "^@@ Host (.*)$" $line -> xhost]} return
+    # += $xhost
+    # FUTURE: Write tests results to a storage back end.
+    #sak::registry::local set [list Tests Results $xhost]
+    io trace Host
+    return -code return
+}
+
+proc ::kettle::Test::Platform {} {
+    upvar 1 line line
+    if {![regexp "^@@ Platform (.*)$" $line -> xplatform]} return
+    io trace Platform
+    # += ($xplatform)
+    #variable xhost
+    #sak::registry::local set $xhost Platform $xplatform
+    return -code return
+}
+
+proc ::kettle::Test::Cwd {} {
+    upvar 1 line line
+    if {![regexp "^@@ CWD (.*)$" $line -> xcwd]} return
+    io trace Cwd
+    #variable xhost
+    #set xcwd [linsert $xhost end $xcwd]
+    #sak::registry::local set $xcwd
+    return -code return
+}
+
+proc ::kettle::Test::Shell {} {
+    upvar 1 line line
+    if {![regexp "^@@ Shell (.*)$" $line -> xshell]} return
+    io trace Shell
+    # += [file tail $xshell]
+    #variable xcwd
+    #set xshell [linsert $xcwd end $xshell]
+    #sak::registry::local set $xshell
+    return -code return
+}
+
+proc ::kettle::Test::Tcl {} {
+    upvar 1 line line
+    if {![regexp "^@@ Tcl (.*)$" $line -> xtcl]} return
+    io trace Tcl
+    #variable xshell
+    #variable maxvl
+    #+= \[$xtcl\][blank [expr {$maxvl - [string length $xtcl]}]]
+    #sak::registry::local set $xshell Tcl $xtcl
+    return -code return
+}
+
+proc ::kettle::Test::Match||Skip||Sourced {} {
+    upvar 1 line line
+    if {[string match "@@ Skip*"                  $line]} {return -code return}
+    if {[string match "@@ Match*"                 $line]} {return -code return}
+    if {[string match "Sourced * Test Files."     $line]} {return -code return}
+    if {[string match "Files with failing tests*" $line]} {return -code return}
+    if {[string match "Number of tests skipped*"  $line]} {return -code return}
+    if {[string match "\[0-9\]*"                  $line]} {return -code return}
+    return
+}
+
+proc ::kettle::Test::Start {} {
+    upvar 1 line line
+    if {![regexp "^@@ Start (.*)$" $line -> start]} return
+    variable xshell
+    #sak::registry::local set $xshell Start $start
+    return -code return
+}
+
+proc ::kettle::Test::End {} {
+    upvar 1 line line
+    if {![regexp "^@@ End (.*)$" $line -> end]} return
+    variable xshell
+    #sak::registry::local set $xshell End $end
+    return -code return
+}
+
+proc ::kettle::Test::StartFile {} {
+    upvar 1 line line
+    if {![regexp "^@@ StartFile (.*)$" $line -> start]} return
+    variable xstartfile $start
+    variable xtestnum 0
+    #sak::registry::local set $xshell Start $start
+    return -code return
+}
+
+proc ::kettle::Test::EndFile {} {
+    upvar 1 line line
+    if {![regexp "^@@ EndFile (.*)$" $line -> end]} return
+    variable xfile
+    variable xstartfile
+    variable xtimes
+    variable xtestnum
+
+    set k [lreplace $xfile 0 3]
+    set k [lreplace $k 2 2 [file tail [lindex $k 2]]]
+    set delta [expr {$end - $xstartfile}]
+
+    if {$xtestnum == 0} {
+	set score $delta
+    } else {
+	# average number of microseconds per test.
+	set score [expr {int(($delta/double($xtestnum))*1000000)}]
+	#set score [expr {$delta/double($xtestnum)}]
+    }
+
+    lappend xtimes $k [list $xtestnum $delta $score]
+
+    variable alog
+    if {$alog} {
+	variable logtim
+	puts $logtim [linsert [linsert $k end $xtestnum $delta $score] 0 TIME]
+    }
+
+    #sak::registry::local set $xshell End $end
+    return -code return
+}
+
+proc ::kettle::Test::Module {} {
+    upvar 1 line line ; variable xmodule
+    if {![regexp "^@@ Module (.*)$" $line -> xmodule]} return
+    variable xshell
+    variable xstatus ok
+    variable maxml
+    += ${xmodule}[blank [expr {$maxml - [string length $xmodule]}]]
+    set xmodule [linsert $xshell end $xmodule]
+    #sak::registry::local set $xmodule
+    return -code return
+}
+
+proc ::kettle::Test::Testsuite {} {
+    upvar 1 line line ; variable xfile
+    if {![regexp "^@@ Testsuite (.*)$" $line -> xfile]} return
+    = <[file tail $xfile]>
+    variable xmodule
+    set xfile [linsert $xmodule end $xfile]
+    #sak::registry::local set $xfile Aborted 0
+    return -code return
+}
+
+proc ::kettle::Test::NoTestsuite {} {
+    upvar 1 line line
+    if {![string match "Error:  No test files remain after*" $line]} return
+    variable xstatus none
+    = {No tests}
+    return -code return
+}
+
+proc ::kettle::Test::Support {} {
+    upvar 1 line line
+    if {![regexp "^- (.*)$" $line -> package]} return
+    #= "S $package"
+    foreach {pn pv} $package break
+    variable xfile
+    #sak::registry::local set [linsert $xfile end Support] $pn $pv
+    return -code return
+}
+
+proc ::kettle::Test::Testing {} {
+    upvar 1 line line
+    if {![regexp "^\\* (.*)$" $line -> package]} return
+    #= "T $package"
+    foreach {pn pv} $package break
+    variable xfile
+    #sak::registry::local set [linsert $xfile end Testing] $pn $pv
+    return -code return
+}
+
+proc ::kettle::Test::Other {} {
+    upvar 1 line line
+    if {![string match ">*" $line]} return
+    return -code return
+}
+
+proc ::kettle::Test::Summary {} {
+    upvar 1 line line
+    if {![regexp "^all\\.tcl:(.*)$" $line -> line]} return
+    variable xmodule
+    variable xstatus
+    variable xvstatus
+    foreach {_ t _ p _ s _ f} [split [string trim $line]] break
+    #sak::registry::local set $xmodule Total   $t ; set t [format %5d $t]
+    #sak::registry::local set $xmodule Passed  $p ; set p [format %5d $p]
+    #sak::registry::local set $xmodule Skipped $s ; set s [format %5d $s]
+    #sak::registry::local set $xmodule Failed  $f ; set f [format %5d $f]
+
+    upvar 2 total _total ; incr _total $t
+    upvar 2 pass  _pass  ; incr _pass  $p
+    upvar 2 skip  _skip  ; incr _skip  $s
+    upvar 2 fail  _fail  ; incr _fail  $f
+    upvar 2 err   _err
+
+    set t [format %5d $t]
+    set p [format %5d $p]
+    set s [format %5d $s]
+    set f [format %5d $f]
+
+    if {$xstatus == "ok" && $t == 0} {
+	set xstatus none
+    }
+
+    set st $xvstatus($xstatus)
+
+    if {$xstatus == "ok"} {
+	# Quick return for ok suite.
+	=| "~~ $st T $t P $p S $s F $f"
+	return -code return
+    }
+
+    # Clean out progress display using a non-highlighted
+    # string. Prevents the char couint from being off. This is
+    # followed by construction and display of the highlighted version.
+
+    = "   $st T $t P $p S $s F $f"
+    switch -exact -- $xstatus {
+	none    {=| "~~ [yel]$st T $t[rst] P $p S $s F $f"}
+	aborted {=| "~~ [whi]$st[rst] T $t P $p S $s F $f"}
+	error   {
+	    =| "~~ [mag]$st[rst] T $t P $p S $s F $f"
+	    incr _err
+	}
+	fail    {=| "~~ [red]$st[rst] T $t P $p S $s [red]F $f[rst]"}
+    }
+    return -code return
+}
+
+proc ::kettle::Test::TestStart {} {
+    upvar 1 line line
+    if {![string match {---- * start} $line]} return
+    set testname [string range $line 5 end-6]
+    = "---- $testname"
+    variable xfile
+    variable xtest [linsert $xfile end $testname]
+    variable xtestnum
+    incr     xtestnum
+    return -code return
+}
+
+proc ::kettle::Test::TestSkipped {} {
+    upvar 1 line line
+    if {![string match {++++ * SKIPPED:*} $line]} return
+    regexp {^[^ ]* (.*)SKIPPED:.*$} $line -> testname
+    set              testname [string trim $testname]
+    variable xtest
+    = "SKIP $testname"
+    if {$xtest == {}} {
+	variable xfile
+	set xtest [linsert $xfile end $testname]
+    }
+    #sak::registry::local set $xtest Status Skip
+    set xtest {}
+    return -code return
+}
+
+proc ::kettle::Test::TestPassed {} {
+    upvar 1 line line
+    if {![string match {++++ * PASSED} $line]} return
+    set             testname [string range $line 5 end-7]
+    variable xtest
+    = "PASS $testname"
+    if {$xtest == {}} {
+	variable xfile
+	set xtest [linsert $xfile end $testname]
+    }
+    #sak::registry::local set $xtest Status Pass
+    set xtest {}
+    return -code return
+}
+
+proc ::kettle::Test::TestFailed {} {
+    upvar 1 line line
+    if {![string match {==== * FAILED} $line]} return
+    set        testname [lindex [split [string range $line 5 end-7]] 0]
+    = "FAIL $testname"
+    variable xtest
+    if {$xtest == {}} {
+	variable xfile
+	set xtest [linsert $xfile end $testname]
+    }
+    #sak::registry::local set $xtest Status Fail
+    ## CAPTURE INIT
+    variable xcollect  1
+    variable xbody     ""
+    variable xactual   ""
+    variable xexpected ""
+    variable xstatus   fail
+    # Ignore failed status if we already have it, or an error
+    # status. The latter is more important to show. We do override
+    # status 'aborted'.
+    if {$xstatus == "ok"}      {set xstatus fail}
+    if {$xstatus == "aborted"} {set xstatus fail}
+    return -code return
+}
+
+proc ::kettle::Test::CaptureFailureSync {} {
+    variable xcollect
+    if {$xcollect != 1} return
+    upvar 1 line line
+    if {![string match {==== Contents*} $line]} return
+    set xcollect 2
+    return -code return
+}
+
+proc ::kettle::Test::CaptureFailureCollectBody {} {
+    variable xcollect
+    if {$xcollect != 2} return
+    upvar 1 rline line
+    variable xbody
+    if {[string match {---- Result was*} $line]} {
+	set xcollect 3
+	return -code return
+    } elseif {[string match {---- Test generated error*} $line]} {
+	set xcollect 5
+	return -code return
+    }
+
+    variable xbody
+    append   xbody $line \n
+    return -code return
+}
+
+proc ::kettle::Test::CaptureFailureCollectActual {} {
+    variable xcollect
+    if {$xcollect != 3} return
+    upvar 1 rline line
+    if {![string match {---- Result should*} $line]} {
+	variable xactual
+	append   xactual $line \n
+    } else {
+	set xcollect 4
+    }
+    return -code return
+}
+
+proc ::kettle::Test::CaptureFailureCollectExpected {} {
+    variable xcollect
+    if {$xcollect != 4} return
+    upvar 1 rline line
+    if {![string match {==== *} $line]} {
+	variable xexpected
+	append   xexpected $line \n
+    } else {
+	variable alog
+	if {$alog} {
+	    variable logfad
+	    variable xtest
+	    variable xbody
+	    variable xactual
+	    variable xexpected
+
+	    puts  $logfad "==== [lrange $xtest end-1 end] FAILED ========="
+	    puts  $logfad "==== Contents of test case:\n"
+	    puts  $logfad $xbody
+
+	    puts  $logfad "---- Result was:"
+	    puts  $logfad [string range $xactual 0 end-1]
+
+	    puts  $logfad "---- Result should have been:"
+	    puts  $logfad [string range $xexpected 0 end-1]
+
+	    puts  $logfad "==== [lrange $xtest end-1 end] ====\n\n"
+	    flush $logfad
+	}
+	set xcollect 0
+	#sak::registry::local set $xtest Body     $xbody
+	#sak::registry::local set $xtest Actual   $xactual
+	#sak::registry::local set $xtest Expected $xexpected
+	set xtest {}
+    }
+    return -code return
+}
+
+proc ::kettle::Test::CaptureFailureCollectError {} {
+    variable xcollect
+    if {$xcollect != 5} return
+    upvar 1 rline line
+    variable xbody
+    if {[string match {---- errorCode*} $line]} {
+	set xcollect 4
+	return -code return
+    }
+
+    variable xactual
+    append   xactual $line \n
+    return -code return
+}
+
+proc ::kettle::Test::Aborted {} {
+    upvar 1 line line
+    if {![string match {Aborting the tests found *} $line]} return
+    variable xfile
+    variable xstatus
+    # Ignore aborted status if we already have it, or some other error
+    # status (like error, or fail). These are more important to show.
+    if {$xstatus == "ok"} {set xstatus aborted}
+    = Aborted
+    #sak::registry::local set $xfile Aborted {}
+    return -code return
+}
+
+proc ::kettle::Test::AbortCause {} {
+    upvar 1 line line
+    if {
+	![string match {Requiring *} $line] &&
+	![string match {Error in *} $line]
+    } return ; # {}
+    variable xfile
+    = $line
+    #sak::registry::local set $xfile Aborted $line
+    return -code return
+}
+
+proc ::kettle::Test::CaptureStackStart {} {
+    upvar 1 line line
+    if {![string match {@+*} $line]} return
+    variable xstackcollect 1
+    variable xstack        {}
+    variable xstatus       error
+    = {Error, capturing stacktrace}
+    return -code return
+}
+
+proc ::kettle::Test::CaptureStack {} {
+    variable xstackcollect
+    if {!$xstackcollect} return
+    upvar 1 line line
+    variable xstack
+    if {![string match {@-*} $line]} {
+	append xstack [string range $line 2 end] \n
+    } else {
+	set xstackcollect 0
+	variable xfile
+	variable alog
+	#sak::registry::local set $xfile Stacktrace $xstack
+	if {$alog} {
+	    variable logerd
+	    puts  $logerd "[lindex $xfile end] StackTrace"
+	    puts  $logerd "========================================"
+	    puts  $logerd $xstack
+	    puts  $logerd "========================================\n\n"
+	    flush $logerd
+	}
+    }
+    return -code return
+}
+
+proc ::kettle::Test::SetupError {} {
+    upvar 1 line line
+    if {![string match {SETUP Error*} $line]} return
+    variable xstatus error
+    = {Setup error}
+    return -code return
+}
+
+# ###
+
+proc ::kettle::Test::+= {string} {
+    variable araw
+    if {$araw} return
+    variable aprefix
+    append   aprefix " " $string
+    sak::animate::next $aprefix
+    return
+}
+
+proc ::kettle::Test::= {string} {
+    variable araw
+    if {$araw} return
+    variable aprefix
+    sak::animate::next "$aprefix $string"
+    return
+}
+
+proc ::kettle::Test::=| {string} {
+    variable araw
+    if {$araw} return
+    variable aprefix
+    sak::animate::last "$aprefix $string"
+    variable alog
+    if {$alog} {
+	variable logsum
+	variable logfai
+	variable logski
+	variable lognon
+	variable xstatus
+	puts $logsum "$aprefix $string" ; flush $logsum
+	switch -exact -- $xstatus {
+	    error   -
+	    fail    {puts $logfai "$aprefix $string" ; flush $logfai}
+	    none    {puts $lognon "$aprefix $string" ; flush $lognon}
+	    aborted {puts $logski "$aprefix $string" ; flush $logski}
+	}
+    }
+    set aprefix ""
+    return
+}
+
+# ###
+
+namespace eval ::kettle::Test {
+    # State of test processing. ... TODO: Goes into a dictionary in 'Run'.
+
+    variable xstackcollect 0
+    variable xstack    {}
+    variable xcollect  0
+    variable xbody     {}
+    variable xactual   {}
+    variable xexpected {}
+    variable xhost     {}
+    variable xplatform {}
+    variable xcwd      {}
+    variable xshell    {}
+    variable xmodule   {}
+    variable xfile     {}
+    variable xtest     {}
+    variable xstartfile {}
+    variable xtimes     {}
+
+    variable xstatus ok
+
+    # Animation prefix of test processing, and flag controlling the
+    # nature of logging (raw vs animation).
+
+    variable aprefix   {}
+    variable araw      0
+
+    # Max length of module names and patchlevel information.
+
+    variable maxml 0
+    variable maxvl 0
+
+    # Map from internal stati to the displayed human readable
+    # strings. This includes the trailing whitespace needed for
+    # vertical alignment.
+
+    variable  xvstatus
+    array set xvstatus {
+	ok      {     }
+	none    {None }
+	aborted {Skip }
+	error   {ERR  }
+	fail    {FAILS}
+    }
+}
 
 # # ## ### ##### ######## ############# #####################
 return
