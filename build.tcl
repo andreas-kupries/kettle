ddb2b3190d4c3877fa7b713e80ef27b977b330fb
Andreas Kupries <akupries@shaw.ca>
2013-12-03 06:32:07 +0000

    Fixed path generation for critcl install, need relative paths.

--- build.tcl
+++ build.tcl
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -*- tcl -*- \
-exec ./kettle "$0" "${1+$@}"
+exec ./kettle -f "$0" "${1+$@}"
 # For kettle sources, documentation, etc. see
 # - http://core.tcl.tk/akupries/kettle
 # - http://chiselapp.com/user/andreas_kupries/repository/Kettle
