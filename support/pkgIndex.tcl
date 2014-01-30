c8006471d8a9680968a7bfdcc096bb77500b8e7a
Andreas Kupries <akupries@shaw.ca>
2014-01-30 18:15:17 +0000

    Updated local copy of Cmdr.

--- support/pkgIndex.tcl
+++ support/pkgIndex.tcl
@@ -2,7 +2,7 @@
 package ifneeded cmdr                   1 [list source [file join $dir cmdr.tcl]]
 package ifneeded cmdr::actor            1 [list source [file join $dir actor.tcl]]
 package ifneeded cmdr::config           1 [list source [file join $dir config.tcl]]
-package ifneeded cmdr::help             1 [list source [file join $dir help.tcl]]
+package ifneeded cmdr::help         1.0.1 [list source [file join $dir help.tcl]]
 package ifneeded cmdr::help::json       1 [list source [file join $dir help_json.tcl]]
 package ifneeded cmdr::help::sql        1 [list source [file join $dir help_sql.tcl]]
 package ifneeded cmdr::officer          1 [list source [file join $dir officer.tcl]]
