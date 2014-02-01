eda1e07f334d3d9ea17bceb7210e50227699b969
Andreas Kupries <akupries@shaw.ca>
2014-02-01 01:22:06 +0000

    Force use of the local copy of cmdr, to ease development regardng bugs and features we may have to fix/implement

--- cli.tcl
+++ cli.tcl
@@ -7,6 +7,24 @@
 ## Try to use whatever the installation supplies first.
 ## On failure fall back to the local copy of the framework.
 
+# Force use of local Cmdr framework.
+# Enables local fixing of bugs before transfering to the official sources.
+# Ditto for cmdr extensions.
+if 1 {apply {dir {
+    source [file join $dir util.tcl]
+    source [file join $dir vcommon.tcl]
+    source [file join $dir validate.tcl]
+    source [file join $dir help.tcl]
+    #source [file join $dir help_json.tcl]
+    #source [file join $dir help_sql.tcl]
+    source [file join $dir parameter.tcl]
+    source [file join $dir config.tcl]
+    source [file join $dir actor.tcl]
+    source [file join $dir private.tcl]
+    source [file join $dir officer.tcl]
+    source [file join $dir cmdr.tcl]
+}} [kettle option get @kettledir]/support}
+
 try {
     package require cmdr
     #kettle io trace {outside cmdr = [package ifneeded cmdr [package present cmdr]]}
